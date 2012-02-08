482a97b14ea2ea406818d60c79090a1177380b94
penumbra <penumbraposts@yahoo.com>
2012-02-08 18:54:20 -0500

    moved require for JAXB code books.jar here

--- lib/example/books.rb
+++ lib/example/books.rb
@@ -1,4 +1,9 @@
-# jaxb bindings for books.xsd
+require 'java'
+
+# require the generated and compiled JAXB code to interact with the books.xsd format
+require 'books.jar'
+
+# Ruby classes to simplify use of JAXB compiled Java objects
 require 'example/books/books.rb'
 require 'example/books/authors.rb'
 require 'example/books/book.rb'
