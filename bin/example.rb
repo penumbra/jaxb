9a8f032cb03759e450d837281bef53e1df736020
penumbra <penumbraposts@yahoo.com>
2012-02-08 18:33:54 -0500

    added ant task for building jar and jar subfolder usage

--- bin/example.rb
+++ bin/example.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env jruby
 
 lib_dir = File.expand_path('../../lib', __FILE__)
+jar_dir = File.expand_path('../../jars', __FILE__)
 
-$LOAD_PATH << lib_dir
+$LOAD_PATH << lib_dir << jar_dir
 $LOAD_PATH << '/tools/jaxb-ri-20110601/lib'
 
 require 'example'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift '/tools/jaxb-ri-20110601/lib'
+lib_dir = File.expand_path('../../lib', __FILE__)
+jar_dir = File.expand_path('../../jars', __FILE__)
+
+$LOAD_PATH << lib_dir << jar_dir
+$LOAD_PATH << '/tools/jaxb-ri-20110601/lib'
 
 require 'example/books.rb'
