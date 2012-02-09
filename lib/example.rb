778a2d060dcbade946d40cae0234c1041fc033d4
penumbra <penumbraposts@yahoo.com>
2012-02-08 22:45:21 -0500

    separate data from example code, use OpenStruct instead of hashes

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,5 @@
 require 'example/books.rb'
+require 'example_data.rb'
 
 class Example
   attr_reader :books   # represents a collection of books
@@ -35,39 +36,8 @@ end
 
 ex = Example.new
 
-agile_rails = { 
-  :item_id => '307', 
-  :name => 'Agile Web Development with Rails', 
-  :isbn => 9780977616633, 
-  :price => '$29.95', 
-  :authors => ['Sam Ruby', 'Dave Thomas', 'David Heinemeier Hansson'],
-  :description => 'Ruby on Rails is a framework that makes it easier to develop, deploy, ' + 
-    'and maintain web applications. During the months that followed its ' +
-    'initial release, Rails went from being an unknown toy to being a ' +
-    'worldwide phenomenon, and more important, it has become the ' +
-    'framework of choice for the implementation of a wide range of ' +
-    'so-called Web 2.0 applications.'}
-
-metaprogramming_ruby = { 
-  :item_id => '308', 
-  :name => 'Metaprogramming Ruby', 
-  :isbn => 1934356476, 
-  :price => '$39.95', 
-  :authors => ['Paulo Perrotta'],
-  :description => 'Ruby inherits characteristics from various languages — Lisp, Smalltalk ' +
-    ', C, and Perl, to name a few.  Metaprogramming comes from Lisp ' +
-    '(and Smalltalk).  It’s a bit like magic, which makes something ' +
-    'astonishing possible.'}
-
-programming_semantic_web = { 
-  :item_id => '309', 
-  :name => 'Programming the Semantic Web', 
-  :isbn => 596153813, 
-  :price => '$19.95', 
-  :authors => ['Toby Segaram', 'Colin Evans', 'Jamie Taylor'] }
-
-ex.add( agile_rails )
-ex.add( metaprogramming_ruby )            
-ex.add( programming_semantic_web )
+ex.add( ExampleData::book1 )
+ex.add( ExampleData::book2 )
+ex.add( ExampleData::book3 )
 
 ex.write_xml 'books.xml'
---
+++ lib/example_data.rb
@@ -0,0 +1,46 @@
+require 'ostruct'
+
+class ExampleData
+  def ExampleData.book1
+    ar = OpenStruct.new
+    ar.item_id = '307'
+    ar.name = 'Agile Web Development with Rails'
+    ar.isbn =  9780977616633 
+    ar.price = '$29.95' 
+    ar.authors = ['Sam Ruby', 'Dave Thomas', 'David Heinemeier Hansson']
+    ar.description = 'Ruby on Rails is a framework that makes it easier to develop, deploy, ' + 
+    'and maintain web applications. During the months that followed its ' +
+    'initial release, Rails went from being an unknown toy to being a ' +
+    'worldwide phenomenon, and more important, it has become the ' +
+    'framework of choice for the implementation of a wide range of ' +
+    'so-called Web 2.0 applications.'
+
+    ar.marshal_dump
+  end
+
+  def ExampleData.book2
+    mr = OpenStruct.new
+    mr.item_id = '308'
+    mr.name = 'Metaprogramming Ruby'
+    mr.isbn = 1934356476 
+    mr.price = '$39.95' 
+    mr.authors = ['Paulo Perrotta']
+    mr.description = 'Ruby inherits characteristics from various languages — Lisp, Smalltalk ' +
+    ', C, and Perl, to name a few.  Metaprogramming comes from Lisp ' +
+    '(and Smalltalk).  It’s a bit like magic, which makes something ' +
+    'astonishing possible.'
+
+    mr.marshal_dump
+  end
+
+  def ExampleData.book3
+    psw = OpenStruct.new
+    psw.item_id = '309'
+    psw.name = 'Programming the Semantic Web'
+    psw.isbn = 596153813
+    psw.price = '$19.95'
+    psw.authors = ['Toby Segaram', 'Colin Evans', 'Jamie Taylor']
+
+    psw.marshal_dump
+  end
+end
