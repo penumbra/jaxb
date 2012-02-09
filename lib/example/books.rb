a02cb2ba78cec28f7da07a104ec5a6cbfa71217e
penumbra <penumbraposts@yahoo.com>
2012-02-09 00:47:56 -0500

    added require promotion

--- lib/example/books.rb
+++ lib/example/books.rb
@@ -6,4 +6,5 @@ require 'books.jar'
 # Ruby classes to simplify use of JAXB compiled Java objects
 require 'example/books/books.rb'
 require 'example/books/authors.rb'
+require 'example/books/promotion.rb'
 require 'example/books/book.rb'
