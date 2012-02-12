527312d661edbe05afca53b17879fc0e0d5e94bf
penumbra <penumbraposts@yahoo.com>
2012-02-12 02:10:35 -0500

    pattern moved a parameter

--- lib/jaxb/xml_date.rb
+++ lib/jaxb/xml_date.rb
@@ -11,8 +11,7 @@ java_import 'java.util.Date'
 module Jaxb
   class XmlDate
     class << self
-      def parse( date_str )
-        pattern = "yyyy-MM-dd"
+      def parse( date_str, pattern = 'yyyy-MM-dd' )
         date = SimpleDateFormat.new( pattern ).parse( date_str )
 
         gcal = GregorianCalendar.new( 1900 + date.year, date.month, date.date)
